857c1841b432e940536562947629f677f7f58f0f
Sean C Davis <scdavis41@gmail.com>
2022-05-31 11:30:27 -0400

    Add contentful starter and example

--- config.js
+++ config.js
@@ -7,11 +7,21 @@ const starters = [
     name: "ts-nextjs",
     repoUrl: "https://github.com/stackbit-themes/ts-mui-nextjs-starter",
   },
+  {
+    name: "contentful",
+    repoUrl: "https://github.com/stackbit-themes/contentful-starter",
+  },
 ];
 
 const examples = {
   repoUrl: "https://github.com/stackbit-themes/stackbit-examples",
-  directories: ["algolia-search", "dynamic-app", "sb-countdown", "sb-typist"],
+  directories: [
+    "algolia-search",
+    "dynamic-app",
+    "sb-countdown",
+    "sb-typist",
+    "tutorial-contentful",
+  ],
 };
 
 export default {
